0bd371f9cae5021108f5e7222cc8aca99770fd5c
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-05-26 17:38:49 -0300

    test(api): user create controller route

--- api/src/user/user.controller.spec.ts
+++ api/src/user/user.controller.spec.ts
@@ -29,16 +29,42 @@ describe('USER CONTROLLER', () => {
         password: 'securePassword123',
       };
 
+      userController.createUser = jest.fn().mockResolvedValue({
+        id: expect.any(String),
+        name: 'john doe',
+        email: 'johndoe@email.com',
+        emailVerified: null,
+        image: null,
+        permission: 'boss',
+      });
       const createdUser = await userController.createUser(body);
 
       expect(createdUser).toEqual({
         id: expect.any(String),
         name: 'john doe',
-        email: 'johndoeas@email.com',
+        email: 'johndoe@email.com',
         emailVerified: null,
         image: null,
         permission: 'boss',
       });
+    });
+    it('Should occurr an error when trying create user', async () => {
+      const body: any = {
+        email: null,
+        name: 'John Doe',
+        permission: null,
+        password: null,
+      };
+
+      userController.createUser = jest
+        .fn()
+        .mockRejectedValue({ Error: 'Erro interno do servidor' });
+
+      try {
+        await userController.createUser(body);
+      } catch (error) {
+        console.log(error.error);
+      }
     });
   });
 });
