a61f8dbe18f563cba00e08180918e05d96706672
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-06-03 19:27:06 -0300

    refactor(API): refactor the authentication guard code add more security and validations

--- api/src/guards/auth.guards.ts
+++ api/src/guards/auth.guards.ts
@@ -3,14 +3,17 @@ import { Request } from 'express';
 import { AuthService } from 'src/auth/auth.service';
 import { FindUserDTO } from 'src/user/dto/find-user.dto';
 import { UserService } from 'src/user/user.service';
+import { Logger } from '@nestjs/common';
 
 interface AuthenticatedRequest extends Request {
-  tokenPayload?: object;
+  tokenPayload?: FindUserDTO;
   user?: FindUserDTO;
 }
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly logger = new Logger(AuthGuard.name);
+
   constructor(
     private readonly authService: AuthService,
     private readonly userService: UserService,
@@ -20,16 +23,38 @@ export class AuthGuard implements CanActivate {
     const request: AuthenticatedRequest = context.switchToHttp().getRequest();
     const { authorization } = request.headers;
 
-    try {
-      const data: FindUserDTO = await this.authService.handleCheckToken(
-        (authorization ?? '').split(' ')[1],
-      );
+    if (!authorization) {
+      this.logger.warn('O cabeçalho de autorização está ausente');
+      return false;
+    }
 
-      request.tokenPayload = data;
-      request.user = await this.userService.findOneById(data.id);
+    const token = authorization.split(' ')[1];
+
+    if (!token) {
+      this.logger.warn('O token de autorização está ausente');
+      return false;
+    }
+
+    try {
+      const tokenPayload: FindUserDTO =
+        await this.authService.handleCheckToken(token);
+
+      request.tokenPayload = tokenPayload;
+      request.user = await this.userService.findOneById(tokenPayload.id);
+
+      if (!request.user) {
+        this.logger.warn(
+          `O usuário com o id '${tokenPayload.id}' não foi encontrado`,
+        );
+        return false;
+      }
 
       return true;
     } catch (error) {
+      this.logger.error(
+        'Ocorreu um erro, verifique se o token, está correto',
+        error.stack,
+      );
       return false;
     }
   }
