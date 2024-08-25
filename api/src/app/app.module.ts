e68347d3f22e6dcc3137ff470a179696691171ca
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2024-08-25 09:09:57 -0300

    refactor(API): refactored and removed undue prismaServices injections

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
